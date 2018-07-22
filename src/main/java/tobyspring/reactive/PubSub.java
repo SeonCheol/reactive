6eca604abe5801c98390a4bd58313d0c1ebfdffe
seoncheol.kim <seoncheol.kim@navercorp.com>
2018-07-22 17:01:52 +0900

    병렬처리로 구현

--- src/main/java/tobyspring/reactive/PubSub.java
+++ src/main/java/tobyspring/reactive/PubSub.java
@@ -2,7 +2,10 @@ package tobyspring.reactive;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Flow.*;
+import java.util.concurrent.Future;
 
 /**
  * @author seoncheol
@@ -15,6 +18,8 @@ public class PubSub {
          */
 
         Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
+        ExecutorService es = Executors.newSingleThreadExecutor(); // for 병렬처리
+
         Publisher p = new Publisher() {
             Iterator<Integer> it = itr.iterator();
 
@@ -24,23 +29,29 @@ public class PubSub {
                     //
                     @Override
                     public void request(long n) {
-                        try {
-                            while (n-- > 0) {
-                                if (it.hasNext()) {
-                                    subscriber.onNext(it.next());
-                                } else {
-                                    subscriber.onComplete();
-                                    break;
+
+                        es.execute(() -> {
+//                        Future<?> future = es.submit(() -> {
+                            try {
+                                int i = 0;
+//                                while (n-- > 0) {
+                                while(i++ < n)  {
+                                    if (it.hasNext()) {
+                                        subscriber.onNext(it.next());
+                                    } else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
                                 }
+                            } catch (RuntimeException e) {
+                                subscriber.onError(e);
                             }
-                        } catch (RuntimeException e) {
-                            subscriber.onError(e);
-                        }
+                        });
                     }
 
                     @Override
                     public void cancel() {
-
+                        System.out.println("cancel");
                     }
                 });
             }
@@ -55,12 +66,12 @@ public class PubSub {
                 System.out.println("onSubscribe()");
 //                subscription.request(Long.MAX_VALUE);
                 this.subscription = subscription;
-                this.subscription.request(2);
+                this.subscription.request(1);
             }
 
             @Override
             public void onNext(Integer item) {
-                System.out.println("onNext " + item);
+                System.out.println(Thread.currentThread().getName() + "-onNext " + item);
                 this.subscription.request(1);
             }
 
@@ -71,7 +82,8 @@ public class PubSub {
 
             @Override
             public void onComplete() {
-                System.out.println("onComplete()");
+                System.out.println(Thread.currentThread().getName() + "-onComplete()");
+                es.shutdown();
             }
         };
 
