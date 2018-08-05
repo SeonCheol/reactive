b350e4aef56f2a77cd7935b29095b53ea68738c0
seoncheol.kim <seoncheol.kim@navercorp.com>
2018-08-05 12:46:09 +0900

    리액티브 1편 코드 및 마크다운 완료

--- src/main/java/tobyspring/reactive/PubSub.java
+++ src/main/java/tobyspring/reactive/PubSub.java
@@ -9,87 +9,183 @@ import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author seoncheol
  */
 public class PubSub {
-    public static void main(String[] args) {
-        // Publisher  <- Observable
-        /*
-         * A Publisher is a provider of a potentially unbounded number of sequenced elements, publishing them according to the demand received from its Subscriber(s).
-         */
+	public static void main(String[] args) throws InterruptedException {
+		// Publisher  <- Observable
+		/*
+		 * A Publisher is a provider of a potentially unbounded number of sequenced elements, publishing them according to the demand received from its Subscriber(s).
+		 */
+		//    	Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
+		//    	
+		//    	Publisher p = new Publisher() {
+		//    		@Override
+		//    		public void subscribe(Subscriber subscriber)	{
+		//    			subscriber.onSubscribe(new Subscription() {
+		//					
+		//					@Override
+		//					public void request(long n) {
+		//					}
+		//					
+		//					@Override
+		//					public void cancel() {
+		//					}
+		//				});
+		//    		}
+		//		};
+		//		Subscriber s = new Subscriber() {
+		//
+		//			@Override
+		//			public void onSubscribe(Subscription s) {
+		//			}
+		//
+		//			@Override
+		//			public void onNext(Object t) {
+		//			}
+		//
+		//			@Override
+		//			public void onError(Throwable t) {
+		//			}
+		//
+		//			@Override
+		//			public void onComplete() {
+		//			}
+		//		};
+		//    	
+		//    	p.subscribe(s);
 
-        Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
-        ExecutorService es = Executors.newSingleThreadExecutor(); // for 병렬처리
+//		Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
+//
+//		Publisher p = new Publisher() {
+//			@Override
+//			public void subscribe(Subscriber subscriber) {
+//				subscriber.onSubscribe(new Subscription() {
+//					Iterator<Integer> it = itr.iterator();
+//
+//					@Override
+//					public void request(long n) {
+//						while (n-- > 0) {
+//							if (it.hasNext()) {
+//								subscriber.onNext(it.next());
+//							} else {
+//								subscriber.onComplete();
+//								break;
+//							}
+//						}
+//					}
+//
+//					@Override
+//					public void cancel() {
+//					}
+//				});
+//			}
+//		};
+//		Subscriber s = new Subscriber() {
+//			// Subscription 저장
+//			Subscription subscription;
+//			@Override
+//			public void onSubscribe(Subscription s) {
+//				System.out.println("onSubscribe");
+//				// s.request(Long.MAX_VALUE);
+//				this.subscription = s;
+//				this.subscription.request(1);
+//			}
+//
+//			@Override
+//			public void onNext(Object t) {
+//				System.out.println("onNext" + t);
+//				 this.subscription.request(1); //  
+//			}
+//
+//			@Override
+//			public void onError(Throwable t) {
+//				System.out.println("onError");
+//			}
+//
+//			@Override
+//			public void onComplete() {
+//				System.out.println("onComplete");
+//			}
+//		};
+//
+//		p.subscribe(s);
 
-        Publisher p = new Publisher() {
-            Iterator<Integer> it = itr.iterator();
-
-            @Override
-            public void subscribe(Subscriber subscriber) {
-                subscriber.onSubscribe(new Subscription() {
-                    //
-                    @Override
-                    public void request(long n) {
-
-                        es.execute(() -> {
-//                        Future<?> future = es.submit(() -> {
-                            try {
-                                int i = 0;
-//                                while (n-- > 0) {
-                                while (i++ < n) {
-                                    if (it.hasNext()) {
-                                        subscriber.onNext(it.next());
-                                    } else {
-                                        subscriber.onComplete();
-                                        break;
-                                    }
-                                }
-                            } catch (RuntimeException e) {
-                                subscriber.onError(e);
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void cancel() {
-                        System.out.println("cancel");
-                    }
-                });
-            }
-        };
-        // Subscriber <- Observer
-        Subscriber<Integer> s = new Subscriber<Integer>() {
-            Subscription subscription;
-
-            // 필수
-            @Override
-            public void onSubscribe(Subscription subscription) {
-                System.out.println("onSubscribe()");
-//                subscription.request(Long.MAX_VALUE);
-                this.subscription = subscription;
-                this.subscription.request(1);
-            }
-
-            @Override
-            public void onNext(Integer item) {
-                System.out.println(Thread.currentThread().getName() + "-onNext " + item);
-                this.subscription.request(1);
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println("onError");
-            }
-
-            @Override
-            public void onComplete() {
-                System.out.println(Thread.currentThread().getName() + "-onComplete()");
-                es.shutdown();
-            }
-        };
-
-        p.subscribe(s);
-    }
+		        Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
+		        ExecutorService es = Executors.newSingleThreadExecutor(); // for 병렬처리
+		
+		        Publisher p = new Publisher() {
+		            Iterator<Integer> it = itr.iterator();
+		
+		            @Override
+		            public void subscribe(Subscriber subscriber) {
+		                subscriber.onSubscribe(new Subscription() {
+		                    //
+		                    @Override
+		                    public void request(long n) {
+		
+		                        es.execute(() -> {
+		//                        Future<?> future = es.submit(() -> {
+		                            try {
+		                                int i = 0;
+		//                                while (n-- > 0) { // 람다 식 밖에서 정의된 변수 사용 안됨.
+		                                while (i++ < n) {
+		                                    if (it.hasNext()) {
+		                                        subscriber.onNext(it.next());
+		                                    } else {
+		                                        subscriber.onComplete();
+		                                        break;
+		                                    }
+		                                }
+		                            } catch (RuntimeException e) {
+		                                subscriber.onError(e);
+		                            }
+		                        });
+		                    }
+		
+		                    @Override
+		                    public void cancel() {
+		                        System.out.println("cancel");
+		                    }
+		                });
+		            }
+		        };
+		        // Subscriber <- Observer
+		        Subscriber<Integer> s = new Subscriber<Integer>() {
+		            Subscription subscription;
+		
+		            // 필수
+		            @Override
+		            public void onSubscribe(Subscription subscription) {
+		                System.out.println("onSubscribe()");
+		//                subscription.request(Long.MAX_VALUE);
+		                this.subscription = subscription;
+		                this.subscription.request(1);
+		            }
+		
+		            @Override
+		            public void onNext(Integer item) {
+		                System.out.println(Thread.currentThread().getName() + "-onNext " + item);
+		                this.subscription.request(1);
+		            }
+		
+		            @Override
+		            public void onError(Throwable throwable) {
+		                System.out.println("onError");
+		            }
+		
+		            @Override
+		            public void onComplete() {
+		                System.out.println(Thread.currentThread().getName() + "-onComplete()");
+//		                es.shutdown();
+		            }
+		        };
+		
+		        p.subscribe(s);
+		        es.awaitTermination(10, TimeUnit.HOURS);
+		       es.shutdown();
+	}
 }
